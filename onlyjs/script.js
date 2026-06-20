2ade41b1e4ecfd66efa6e0dbaab946bb61b1cada
Umer Shakir <159330908+umer987@users.noreply.github.com>
2026-06-21 00:40:56 +0500

    Update script.js

--- onlyjs/script.js
+++ onlyjs/script.js
@@ -1,4 +1,4 @@
-bzbzbzbsgzgzgzgzbsbbdhshzhshhshshshshshhshhshhshs
+bzbzbzbsgzgzgzgzbsbbdhshzhshhshshshshshhshhshhsh
 // var a ="umer"
 // var b ="shakir"
 // console.log("my name is "+ `${a}`+" " +`${b}`)
